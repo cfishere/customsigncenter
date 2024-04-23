00b27cd16ba0482594ce50fb19ad46fde23cbf93
= <=>
2024-04-23 13:27:54 -0400

    Upgrade Laravel 9.3.1 to ^10

--- public/calendar/PageComponents/CompanyTitle.php
+++
@@ -1,8 +0,0 @@
-<div class="container-fluid">
-	<div class="row">		
-		<div class="col-sm-12">
-			<h2 id="pageTitle" style="color:#7892fa;text-align:center;padding:10px auto 0px auto;margin:0"></h2>
-		</div>
-		
-	</div><!-- /row -->
-</div><!-- /container-fluid -->
